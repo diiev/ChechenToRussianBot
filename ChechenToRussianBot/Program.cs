07445f030995d8743203f6b350b165c8e5f43495
Россия <ramzans095@mail.ru>
2021-02-12 12:43:38 +0300

    Консольный функционал

--- ChechenToRussianBot/Program.cs
+++ ChechenToRussianBot/Program.cs
@@ -7,10 +7,26 @@ namespace ChechenToRussianBot
     {
         static void Main(string[] args)
         {
-            Tutor tutor = new Tutor();
-            tutor.AddWord("ж1ал", "собака");
-            Console.WriteLine(tutor.CheckWord("ж1ал", "собак"));
-            
+            var tutor = new Tutor();
+            tutor.AddWord("ж1аьл", "собака");
+            tutor.AddWord("цициг", "кошка");
+            tutor.AddWord("д1аг1о", "уходи");
+            tutor.AddWord("дахк", "мышь");
+
+            while (true)
+            {
+                var word = tutor.GetRandomChechWord();
+                Console.WriteLine($"Как переводится слово: {word} ");
+                var userAnswer = Console.ReadLine();
+                if (tutor.CheckWord(word,userAnswer))
+                    Console.WriteLine("Правильно");
+                else
+                {
+                    var correctAnswer = tutor.Translate(word);
+                    Console.WriteLine($"Неверно. Правильный ответ: {correctAnswer}");
+
+                }
+            }
         }
     }
 }
